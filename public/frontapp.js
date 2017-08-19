b5039292a87a4e20617370e24c8d9afff3f28687
abk4abhishek <abk4abhishek@gmail.com>
2017-08-19 20:12:24 +0530

    Added functionality to post json body with json validation

--- public/frontapp.js
+++ public/frontapp.js
@@ -2,12 +2,50 @@ var app = angular.module('myfrontapp', []);
 
 app.controller('myMainCtrl', function($scope, $http) {
 
+
+  // var stringConstructor = "test".constructor;
+  // var arrayConstructor = [].constructor;
+  var objectConstructor = {}.constructor;
+
+  function whatIsIt(object) {
+      if (object === null) {
+          return "null";
+      }
+      else if (object === undefined) {
+          return "undefined";
+      }
+      // else if (object.constructor === stringConstructor) {
+      //     return "String";
+      // }
+      // else if (object.constructor === arrayConstructor) {
+      //     return "Array";
+      // }
+      else if (object.constructor == objectConstructor) {
+          return "Object";
+      }
+      else {
+          return "don't know";
+      }
+  }
+
   $scope.HitApi=function(url,method){
+
+
+        if (method=="PUT"){
+          payload={
+            method: method,
+            data:$scope.apibody,
+            url:url
+          }
+        }else if (method=="GET") {
+          payload={
+            method: method,
+            url:url
+          }
+        }
+
         console.log(method);
-        $http({
-          method: method,
-          url:url
-        }).then(function successCallback(response) {
+        $http(payload).then(function successCallback(response) {
           // this callback will be called asynchronously
           // when the response is available
           $scope.status=response.status;
@@ -23,7 +61,30 @@ app.controller('myMainCtrl', function($scope, $http) {
           mytest($scope);
         });
   }
+  function isJSON(str) {
+      try {
+          JSON.parse(str);
+      } catch (e) {
+          return false;
+      }
+      return true;
+  }
+  $scope.apibody = {
+    "client": "abk1",
+    "id": 2
+};
+  $scope.apibodyjson = angular.toJson($scope.apibody);
 
+
+
+  $scope.update = function() {
+    if(isJSON($scope.apibodyjson)){
+            $scope.apibody = angular.fromJson($scope.apibodyjson);
+            $scope.myjsonvalidationmessage="Valid JSON";
+          }else{
+          $scope.myjsonvalidationmessage="Invalid JSON";
+        }
+    }
 });
 
 
--- public/js/first.js
+++ public/js/first.js
@@ -1,5 +1,6 @@
 function redbox1(){
   $( document ).ready(function(){
+    $('.modal').modal();
     $(".button-collapse").sideNav({
   menuWidth: 300, // Default is 240
   closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
